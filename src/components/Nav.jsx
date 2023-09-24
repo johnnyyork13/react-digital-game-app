012d8390514758cadf9d01c6eab843cd56957c33
johnnyyork13 <johnnyyork13@hotmail.com>
2023-09-24 16:46:29 -0400

    add cart logo for illustrating how many items are in cart

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -2,16 +2,24 @@ import React from 'react';
 import '../styles/Nav.css';
 import '../styles/Colors.css';
 import { Link } from 'react-router-dom';
+import cartLogo from '../assets/cart.png';
 
 export default function Nav(props) {
+
+    //style for logo
+    const cartLogoStyle = {
+        backgroundImage: `url(${cartLogo})`
+    }
 
     return (
         <div className="nav">
             <Link to="/" state={{cart: props.cart}}>Back Home</Link>
             <input className="search-bar" type="search" placeholder="Search for games..."/>
             <button className="search-btn" type="button">Search</button>
-            <h1>Items in Cart: {props.cart.length}</h1>
-            <Link to="/cart" state={{cart: props.cart}}>View Cart</Link>
+            <div className="cart-logo" style={cartLogoStyle}>
+                <Link className="cart-count" to="/cart" state={{cart: props.cart}}>{props.cart.length}</Link>
+            </div>
+            
         </div>
     )
 }
